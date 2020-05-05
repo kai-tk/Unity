6030e01aadf4937b7119bf7034002b57a01814c3
tk-aria <aria.kqja@gmail.com>
2020-05-05 23:55:44 +0900

    add Planet

---
+++ Planet/Assets/Scripts/Orbiter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    public GameObject centerObject;
+    public float radius;
+    public float cycle;
+
+    float angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle -= Time.deltaTime * 2 * Mathf.PI / cycle / 50;
+        float x = Mathf.Sin(angle) * radius;
+        float z = Mathf.Cos(angle) * radius;
+
+        Vector3 center = centerObject.transform.position;
+        transform.position = center + new Vector3(x, 0, z);
+    }
+}
